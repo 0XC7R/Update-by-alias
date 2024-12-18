c309c3c937c07241a717bffaf606ca99a62e4f22
TwoBitDev/OXC7R <0xC7R@proton.me>
2024-12-18 18:24:06 +0000

    improvements, wrote while tired

--- updateAllUpgradable.sh
+++ updateAllUpgradable.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-echo "########################################################"
-echo "All options will default to \"n\"/\"no\" type options if \"yes\"/\"y\" is not parsed."
-echo "########################################################\n"
+echo "#####################################################################################"
+echo "#   All options will default to non-yes options if \"yes\"/\"y\" is not parsed.         #"
+echo "#   Parse any input (including empty) if you do not want to do \"yes\"/\"y\" options.   #"
+echo "#####################################################################################"
+
+echo "Update apt sources? (yes/y): "
+read update
+
+# update sources first since we will want to make sure we are able to upgrade them first
+
+if [ "$update" == "yes" ] || [ "$update" == "y" ]; then
+   sudo apt update && clear # change to your systems clear command if clear does not exist which it should :/
+else
+   echo "Not updating list of upgradable packages."
+fi
 
 echo "Grep Phrase (e.g python): "
 read Phrase
@@ -12,6 +24,7 @@ read delOnF
 
 echo "Auto Install? (yes/y): "
 read autoI
+
 
 # get all upgradables from x phrase > loop through each row (pkg1 \n pkg2 etc) from "upgradable.txt"
 sudo apt list --upgradable | grep "$Phrase" | awk -F/ '{print $1}' > upgradable.txt
